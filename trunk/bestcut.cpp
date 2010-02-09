9205740a7b8442da0a22a15294e6d03258d3ef71
sachin.surendran@gmail.com <sachin.surendran@gmail.com@7d0bb44e-1524-11df-8737-d3043b32d176>
2010-02-09 10:22:44 +0000

    Working best fit, gives a magical wastage of 32!

--- trunk/bestcut.cpp
+++ trunk/bestcut.cpp
@@ -7,11 +7,21 @@
 
 #include "genome.h"
 
+#define CHANNEL_LENGTH 50 
+#define FRAMES_TO_BE_CUT  26 
+#define GENE_POOL_SIZE    30
+
+#define MUTATION_RATE 10 
+//                       0    1   2  3   4  5  6  7   8   9  10  11  12  13  14  15  16  17  18  19  20  21 22 23 24  25
+int frame_to_be_cut[] = {23, 24, 25, 3, 27, 1, 1, 1, 20, 20, 24, 43, 37, 28, 30, 33, 33, 33, 50, 10, 23, 26, 2, 3, 7, 24};
+
+
 int main() {
 
 	gdImagePtr im; //declaration of the image
 	int black,white,x,y;
 	time_t time_now;
+
 
 	time_now = time(NULL);
 
@@ -25,8 +35,37 @@ int main() {
 
         my_gene.dump_genome();
 
-        my_gene.randomize_genome();
+        //my_gene.randomize_genome();
+        my_gene.crossover();
         my_gene.dump_genome();
+
+        genome_operator g_op;
+        genome *gene_pool[GENE_POOL_SIZE];
+
+        /* Feed in the params, including channel length */
+
+        g_op.set_channel_size(CHANNEL_LENGTH);
+        g_op.set_req_frame_sizes(frame_to_be_cut, FRAMES_TO_BE_CUT);
+
+        /* Create the gene  pool */
+        g_op.create_genome_pool(gene_pool, GENE_POOL_SIZE, FRAMES_TO_BE_CUT);
+
+        int iteration = 0;
+
+        while (iteration++ < 100000) {
+
+            g_op.sort_gene_pool(gene_pool, GENE_POOL_SIZE);
+
+            g_op.crossover_genepool(gene_pool, GENE_POOL_SIZE, MUTATION_RATE);
+
+            g_op.calc_fitness_of_pool(gene_pool, GENE_POOL_SIZE);
+
+            printf("iteration = %d\n", iteration);
+            //gene_pool[0]->dump_genome();
+
+        }
+
+        gene_pool[0]->dump_genome(frame_to_be_cut, FRAMES_TO_BE_CUT, CHANNEL_LENGTH);
 }
 
 
--- trunk/genome.h
+++ trunk/genome.h
@@ -8,6 +8,9 @@
 #define MUTATION_PROPABLITY 0.0901 /* 50 percent */
 #define MUTATION_FACTOR (RANGE * MUTATION_PROPABLITY) 
 
+/* Number of parents from previous generation to take to next gen */
+#define KEEPALIVE_POPULATION 5 
+
 #define WORST_FITNESS 0xffff
 
 typedef struct pixel {
@@ -54,9 +57,40 @@ class genome{
         void mutate();
         void mutate(float percent);
         void dump_genome();
+        void dump_genome(int *required_frames, int no_of_frames, int channel_size);
         void load_param_to_gene(int *params);
+        void crossover();
 
 };
+
+void genome::crossover()
+{
+    int *tmp_gene_ptr;
+    int crossover_point;
+
+    tmp_gene_ptr = (int *) malloc(sizeof(int) * gene_len);
+
+    bcopy(genome_ptr, tmp_gene_ptr, gene_len * sizeof(int));
+
+    crossover_point = (rand() % gene_len);
+#define DEBUG
+#ifdef DEBUG
+    printf("Crossover point = %d\n", crossover_point);
+#endif /* DEBUG */
+    /* Move segment from crossover point to end of the gene, to start 
+       Note: bcopy(src, dst, len ) */
+    bcopy((void *) (tmp_gene_ptr + crossover_point), (void *)genome_ptr,
+            (gene_len - crossover_point) * sizeof(int));
+    /* Move segment start to crossover to the end */
+    bcopy((void *) (tmp_gene_ptr), (void *) (genome_ptr + (gene_len - crossover_point)),
+            (crossover_point * sizeof(int)));
+
+    free (tmp_gene_ptr);
+}
+
+
+
+
 
 void genome::dump_genome()
 {
@@ -64,6 +98,28 @@ void genome::dump_genome()
     for (int indx = 0; indx < gene_len; indx++) {
         printf(" %d:", *(genome_ptr+indx));
     }
+    printf("\n");
+}
+
+void genome::dump_genome(int *required_frames, int no_of_frames, int channel_size)
+{
+    int used_up_size = 0;
+    int indx = 0;
+    while(indx < gene_len) {
+
+        if (used_up_size + required_frames[*(genome_ptr+indx)] <= channel_size) {
+
+            printf(" %d:", required_frames[*(genome_ptr+indx)]);
+            used_up_size += required_frames[*(genome_ptr+indx)];
+            /* Next frame */
+            indx++;
+        } else {
+            printf ( "\n Next Channel \n");
+            used_up_size = 0;
+        }
+
+    }
+
     printf("\n");
 }
 
@@ -150,7 +206,6 @@ void genome::set_fitness(int new_fitness)
     fitness = new_fitness;
 }
 
-#ifdef DONT_COMPILE
 
 /*
  *
@@ -160,37 +215,54 @@ void genome::set_fitness(int new_fitness)
 
 class genome_operator{
 
-    public:
+    int *required_frames;
+    int channel_size;
 
-        void crossover(genome *parent_gen1,
-                genome *parent_gen2,
-                genome *child_gen1,
-                genome *child_gen2);
-        int calculate_fitness(genome *genome, genome *target_genome);
-        void pixelize(genome *some_genome);
+    public:
+        ~genome_operator();
+        int calculate_fitness(genome *genome);
         void sort_gene_pool(genome *head[], int gene_pool_size);
-        void calc_fitness_of_pool(genome *head[], int gene_pool_size, genome *target_genome);
+        void calc_fitness_of_pool(genome *head[], int gene_pool_size);
         void crossover_genepool(genome *head[], int gene_pool_size);
         void crossover_genepool(genome *head[], int gene_pool_size, float mutation_percent);
-        void create_genome_pool(genome *head[], int gene_pool_size, int gene_X_size, int gene_Y_size);
+        void create_genome_pool(genome *head[], int gene_pool_size, int gene_size);
+        void set_channel_size(int ch_size);
+        void set_req_frame_sizes(int *req_frames, int no_of_frames);
 };
 
-void genome_operator::create_genome_pool(genome *head[], int gene_pool_size, int gene_X_size, int gene_Y_size)
+genome_operator::~genome_operator()
+{
+    free(required_frames);
+}
+
+void genome_operator::set_req_frame_sizes(int *req_frames, int no_of_frames)
+{
+    required_frames = (int *) malloc(no_of_frames * sizeof(int));
+    memcpy(required_frames, req_frames, (no_of_frames * sizeof(int)));
+}
+
+
+void genome_operator::set_channel_size(int ch_size)
+{
+    channel_size = ch_size;
+}
+
+void genome_operator::create_genome_pool(genome *head[], int gene_pool_size, int gene_size)
 {
     for (int count=0; count < gene_pool_size; count++)
     {
-        head[count] = new genome(gene_X_size, gene_Y_size);
+        head[count] = new genome(gene_size);
         head[count]->randomize_genome();
     }
 }
 
-void genome_operator::calc_fitness_of_pool(genome *head[], int gene_pool_size, genome *target_genome)
+void genome_operator::calc_fitness_of_pool(genome *head[], int gene_pool_size)
 {
     int indx;
 
     for (indx = 0; indx < gene_pool_size; indx++)
     {
-        calculate_fitness(head[indx], target_genome);
+        calculate_fitness(head[indx]);
         //printf(" Fitness [%d] = %d\n", indx, head[indx]->get_fitness());
     }
     //printf("---------------------------------\n");
@@ -219,147 +291,67 @@ void genome_operator::sort_gene_pool(genome *head[], int gene_pool_size)
     printf("---------------------------------\n");
     printf(" Best Fitness [%d]  = %d\n", 0, head[0]->get_fitness());
     /* Print fitness of worst genome */
-    printf(" Worst Fitness [%d] = %d\n", 0, head[5]->get_fitness());
+    //printf(" Worst Fitness [%d] = %d\n", 0, head[5]->get_fitness());
     printf("---------------------------------\n");
 #endif
 
 }
 
 
-void genome_operator::pixelize(genome *some_genome)
+int genome_operator::calculate_fitness(genome *some_genome)
 {
-    unsigned char indx = 0;
-    unsigned char i,j;
-    int x = some_genome->x;
-    int y = some_genome->y;
+    int length_usedup = 0;
+    int wastage = 0;
+    int frame_to_be_cut_next;
+    int channel_size_remaining;
+    int indx = 0;
 
-    printf("Pixelize\n");
-
-    if (some_genome->genome_ptr) 
+    while(indx < some_genome->gene_len)
     {
-        for (i=0; i<x; i++) {
-            for (j=0; j<y; j++){
-                //printf(" some_genome->genome_ptr[%d][%d] = %d\n", i, j, (char) (* (some_genome->genome_ptr + (x*i) + j)));
-                indx = 0;
-                while(data[indx++].val != (char) (* (some_genome->genome_ptr + (x*i) + j)) && (indx <= MAX_CHAR) )
-                {
-                    ;
-                }
-                //printf("%02d ", indx-1);
-                if (indx == MAX_CHAR)
-                    printf("%c", data[MAX_CHAR -1].c);
-                else
-                    printf("%c", data[indx-1].c);
+            /* See if we can cut the required frame from current frame */
+            frame_to_be_cut_next = required_frames[*(some_genome->genome_ptr + indx)];
+            channel_size_remaining = channel_size - length_usedup;
 
+            if (frame_to_be_cut_next <= channel_size_remaining)
+            {
+#ifdef DEBUG
+                //printf("%d:Frame size %d cut\n", indx, frame_to_be_cut_next);
+#endif /* DEBUG */
+                length_usedup += frame_to_be_cut_next;
+                /* Move to next frame to be cut */
+                indx++;
+            } else {
+                /* Add rest to wastage */
+                //printf("%d: Wastage = %d\n", indx, wastage);
+                wastage += channel_size_remaining;
+                /* Start with new channel
+                 * NOTE: dont keep track of channels, as wastage tracks he same*/
+                length_usedup = 0;
             }
-            printf("\n");
-        }
     }
+    some_genome->set_fitness(wastage);
+#ifdef DEBUG
+    //printf(" Wastage = %d\n", wastage);
+#endif /* DEBUG */
 }
 
 
-
-int genome_operator::calculate_fitness(genome *some_genome, genome *target_genome)
-{
-    int max_x = some_genome->x;
-    int max_y = some_genome->y;
-    int fitness = 0;
-    int x, y;
-
-
-    for (x = 0; x < max_x; x++)
-    {
-        for (y = 0; y < max_y; y++)
-        {
-            int diff = *(some_genome->genome_ptr + (x * max_x) + y) - *(target_genome->genome_ptr + (x * max_x) + y);
-
-            if (diff > 0)
-                fitness += diff;
-            else
-                fitness -= diff;
-        }
-    }
-    some_genome->set_fitness(fitness);
-    //printf("fitness = %d\n", fitness);
-}
 
 void genome_operator::crossover_genepool(genome *head[], int gene_pool_size)
 {
-    for(int indx = 2; indx < gene_pool_size; indx +=  2)
+    for(int indx = KEEPALIVE_POPULATION; indx < gene_pool_size; indx++)
     {
-        /* Crossover parent1 & 2 and produce all the children */
-        crossover(head[0], head[1],
-                head[indx], head[indx+1]);
+        head[indx]->crossover();
     }
 }
 
 void genome_operator::crossover_genepool(genome *head[], int gene_pool_size, float mutation_percent)
 {
-    for(int indx = 2; indx < gene_pool_size; indx +=  2)
+    for(int indx = KEEPALIVE_POPULATION; indx < gene_pool_size; indx++)
     {
-        /* Crossover parent1 & 2 and produce all the children */
-        crossover(head[0], head[1],
-                head[indx], head[indx+1]);
         head[indx]->mutate(mutation_percent);
-        head[indx+1]->mutate(mutation_percent);
     }
 }
 
 
-void genome_operator::crossover(genome *parent_gen1,
-                        genome *parent_gen2,
-                        genome *child_gen1,
-                        genome *child_gen2)
-{
-    int x,y;
-    /* Get the crossover limits */
-    int crossover_X = (rand() % parent_gen1->x);
-    int crossover_Y = (rand() % parent_gen1->y);
-    int max_x = parent_gen1->x;
-    int max_y = parent_gen1->y;
 
-    for (x = 0; x < crossover_X; x++)
-    {
-        for (y = 0; y < crossover_Y; y++)
-        {
-            *(child_gen1->genome_ptr + (x * max_x) + y) 
-                = *(parent_gen1->genome_ptr + (x*max_x) + y);
-            *(child_gen2->genome_ptr + (x * max_x) + y)
-                = *(parent_gen2->genome_ptr + (x*max_x) + y);
-        }
-    }
-    for (x = crossover_X; x < max_x; x++)
-    {
-        for (y = crossover_Y; y < max_y; y++)
-        {
-            *(child_gen1->genome_ptr + (x * max_x) + y) 
-                = *(parent_gen1->genome_ptr + (x*max_x) + y);
-            *(child_gen2->genome_ptr + (x * max_x) + y)
-                = *(parent_gen2->genome_ptr + (x*max_x) + y);
-        }
-    }
-
-    for (x = 0; x < crossover_X; x++)
-    {
-        for (y = crossover_Y; y < max_y; y++)
-        {
-            *(child_gen1->genome_ptr + (x * max_x) + y)
-                = *(parent_gen2->genome_ptr + (x*max_x) + y);
-            *(child_gen2->genome_ptr + (x * max_x) + y)
-                = *(parent_gen1->genome_ptr + (x*max_x) + y);
-        }
-    }
-
-    for (x = crossover_X; x < max_x; x++)
-    {
-        for (y = 0; y < crossover_Y; y++)
-        {
-            *(child_gen1->genome_ptr + (x * max_x) + y)
-                = *(parent_gen2->genome_ptr + (x*max_x) + y);
-            *(child_gen2->genome_ptr + (x * max_x) + y)
-                = *(parent_gen1->genome_ptr + (x*max_x) + y);
-        }
-    }
-}
-
-#endif /* DONT_COMPILE */ 
